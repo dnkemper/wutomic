38b05f85b67b73a2e3fe96687dd6a9590974d040
Dani Wisnousky <daniellew@wustl.edu>
2026-04-10 06:31:59 -0500

    book fixes

--- web/modules/custom/layout_builder_custom/js/card-block-form.js
+++ web/modules/custom/layout_builder_custom/js/card-block-form.js
@@ -7,7 +7,23 @@
       // We target the .media-library-widget class because there are not a lot of
       // good choices for classes/IDs to target in the inline block form.
       $(once('media-form-attach', '.media-library-widget', context)).each(function () {
+        const checkbox_wrapper = context.querySelector('div[data-drupal-selector$="autoplay-wrapper"]');
+        if (checkbox_wrapper) {
+          // Check if the referenced media is a video.
+          const mediaTypeVideo = context.querySelector('.media--video');
 
+          if (mediaTypeVideo) {
+            // Show the autoplay field.
+            checkbox_wrapper.classList.remove('js-hide');
+            checkbox_wrapper.removeAttribute('tabindex');
+            checkbox_wrapper.removeAttribute('aria-hidden');
+          } else {
+            // Hide the autoplay field.
+            checkbox_wrapper.classList.add('js-hide');
+            checkbox_wrapper.tabIndex = -1;
+            checkbox_wrapper.setAttribute('aria-hidden', 'true');
+          }
+        }
         // Check that we can access the next field.
         const $form_wrapper = $(this).parent().parent().parent();
         const $image_format_field = $form_wrapper.find('.form-item-layout-builder-style-media-format').find('select');
